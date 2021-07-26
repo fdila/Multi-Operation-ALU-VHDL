22cf9f186dde26f4e5bbc832e99f4b5046bccee8
Michele Pugno <michele.pugno@gmail.com>
2021-07-26 15:33:02 +0200

    update

--- FSM.vhd
+++ FSM.vhd
@@ -16,108 +16,108 @@ begin
 
 fsm_flow: process(clk, reset)
 begin
-    if rising_edge(clk) then
-        if enable='1' then
-            case currentstate is 
-                when START =>
-                    case X is 
-                        when '0' => nextstate <= S0;
-                        when '1' => nextstate <= S1;
-                        when others => nextstate <= ERR;
-                    end case;
-                when S0 =>
-                    case X is 
-                        when '0' => nextstate <= S00;
-                        when '1' => nextstate <= S01;
-                        when others => nextstate <= ERR;
-                    end case;
-                when S1 =>
-                    case X is 
-                        when '0' => nextstate <= S10;
-                        when '1' => nextstate <= S11;
-                        when others => nextstate <= ERR;
-                    end case;
-                when S00 =>
-                    case X is 
-                        when '0' => nextstate <= SB;
-                        when '1' => nextstate <= TX;
-                        when others => nextstate <= ERR;
-                    end case;
-                when S01 =>
-                    case X is 
-                        when '0' => nextstate <= ADD;
-                        when '1' => nextstate <= C2;
-                        when others => nextstate <= ERR;
-                    end case;
-                when S10 =>
-                    case X is 
-                        when '0' => nextstate <= SUB;
-                        when '1' => nextstate <= COMP;
-                        when others => nextstate <= ERR;
-                    end case;
-                when S11 =>
-                    case X is 
-                        when '0' => nextstate <= ERR;
-                        when '1' => nextstate <= RX;
-                        when others => nextstate <= ERR;
-                    end case;
-                when SB =>
-                    case X is
-                        when '0' => nextstate <= S0;
-                        when '1' => nextstate <= S1;
-                        when others => nextstate <= ERR;
-                    end case;
-                when TX =>
-                    case X is
-                        when '0' => nextstate <= S0;
-                        when '1' => nextstate <= S1;
-                        when others => nextstate <= ERR;
-                    end case;
-                when ADD =>
-                    case X is
-                        when '0' => nextstate <= S0;
-                        when '1' => nextstate <= S1;
-                        when others => nextstate <= ERR;
-                    end case;
-                when C2 =>
-                    case X is
-                        when '0' => nextstate <= S0;
-                        when '1' => nextstate <= S1;
-                        when others => nextstate <= ERR;
-                    end case;
-                when SUB =>
-                    case X is
-                        when '0' => nextstate <= S0;
-                        when '1' => nextstate <= S1;
-                        when others => nextstate <= ERR;
-                    end case;
-                when COMP =>
-                    case X is
-                        when '0' => nextstate <= S0;
-                        when '1' => nextstate <= S1;
-                        when others => nextstate <= ERR;
-                    end case;
-                when ERR =>
-                    case X is
-                        when '0' => nextstate <= S0;
-                        when '1' => nextstate <= S1;
-                        when others => nextstate <= ERR;
-                    end case;
-                when RX =>
-                    case X is
-                        when '0' => nextstate <= S0;
-                        when '1' => nextstate <= S1;
-                        when others => nextstate <= ERR;
-                    end case;
-            end case;
-        end if; -- end if enable
-        currentstate <= nextstate;
-    end if; -- end if rising_edge
-    
     if reset='0' then
         currentstate <= START;
         finalstate <= "000";
-    end if; -- end if reset
+
+        else if rising_edge(clk) then
+            if enable='1' then
+                case currentstate is 
+                    when START =>
+                        case X is 
+                            when '0' => nextstate <= S0;
+                            when '1' => nextstate <= S1;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when S0 =>
+                        case X is 
+                            when '0' => nextstate <= S00;
+                            when '1' => nextstate <= S01;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when S1 =>
+                        case X is 
+                            when '0' => nextstate <= S10;
+                            when '1' => nextstate <= S11;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when S00 =>
+                        case X is 
+                            when '0' => nextstate <= SB;
+                            when '1' => nextstate <= TX;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when S01 =>
+                        case X is 
+                            when '0' => nextstate <= ADD;
+                            when '1' => nextstate <= C2;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when S10 =>
+                        case X is 
+                            when '0' => nextstate <= SUB;
+                            when '1' => nextstate <= COMP;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when S11 =>
+                        case X is 
+                            when '0' => nextstate <= ERR;
+                            when '1' => nextstate <= RX;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when SB =>
+                        case X is
+                            when '0' => nextstate <= S0;
+                            when '1' => nextstate <= S1;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when TX =>
+                        case X is
+                            when '0' => nextstate <= S0;
+                            when '1' => nextstate <= S1;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when ADD =>
+                        case X is
+                            when '0' => nextstate <= S0;
+                            when '1' => nextstate <= S1;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when C2 =>
+                        case X is
+                            when '0' => nextstate <= S0;
+                            when '1' => nextstate <= S1;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when SUB =>
+                        case X is
+                            when '0' => nextstate <= S0;
+                            when '1' => nextstate <= S1;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when COMP =>
+                        case X is
+                            when '0' => nextstate <= S0;
+                            when '1' => nextstate <= S1;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when ERR =>
+                        case X is
+                            when '0' => nextstate <= S0;
+                            when '1' => nextstate <= S1;
+                            when others => nextstate <= ERR;
+                        end case;
+                    when RX =>
+                        case X is
+                            when '0' => nextstate <= S0;
+                            when '1' => nextstate <= S1;
+                            when others => nextstate <= ERR;
+                        end case;
+                end case;
+            end if; -- end if enable
+        currentstate <= nextstate;
+        end if; -- end else if
+    end if; -- end if 
 end process;
 
 fsm_out: process(currentstate)
